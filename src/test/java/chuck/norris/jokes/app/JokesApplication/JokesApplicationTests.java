0111e8847a0e3bf5ada9985f65c726504e81cb06
Shubham Khetan <12shubhamkhetan@gmail.com>
2021-08-24 13:15:29 +0530

    Initial Commit

---
+++ src/test/java/chuck/norris/jokes/app/JokesApplication/JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package chuck.norris.jokes.app.JokesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
